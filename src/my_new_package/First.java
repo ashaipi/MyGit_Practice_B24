aa6caa45a606efb258b8659a37d1f15531186d95
ashaipi <ashaipi@yahoo.com>
2021-10-03 14:09:02 -0400

    added .gitignore and first java file

--- src/my_new_package/First.java
+++ src/my_new_package/First.java
@@ -2,10 +2,12 @@ package my_new_package;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("If a file created before [Enable Version Control Integration], than we can not commit it");
+        System.out.println("If a file created before [Enable Version Control Integration], then we can not commit it");
 
         // To Do some lines update to the file to can commit it
 
-        System.out.println("first line after enabled VCI");
+        System.out.println("first line after enabled VCS");
+
+        // First update after first commit
     }
 }
